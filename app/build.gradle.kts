f6c6ff77b4f971bc61fd976b4bf9b0e99ae4b045
MJ Luan <angolluan.279455@lipa.sti.edu.ph>
2025-07-27 01:44:11 +0800

    Versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose.compiler)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -14,7 +15,6 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -23,59 +23,45 @@ android {
         viewBinding = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
-
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-    // AndroidX & Material
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    // AndroidX & Material 
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity.compose)
 
-    implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.6.2")
-    // Compose BOM (handles version alignment)
-    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.runtime:runtime")
-    implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.compose.ui:ui-graphics")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-
-    // Required for setContent in ComponentActivity
-    implementation("androidx.activity:activity-compose:1.9.0")
-
-    // Stream Video
-    implementation("io.getstream:stream-video-android-core:1.9.2")
-    implementation("io.getstream:stream-video-android-ui-compose:1.9.2")
-    // implementation("io.getstream:stream-video-android-models:1.9.2") // if needed later
-
-    // Kotlin Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    // Compose
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+    debugImplementation(libs.compose.ui.tooling.debug)
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
 
-    // Glide
-    implementation("com.github.bumptech.glide:glide:4.15.1")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+    // Other libraries
+    implementation(libs.glide)
+    annotationProcessor(libs.glide.compiler)
+    implementation(libs.cloudinary)
 
-    // Cloudinary
-    implementation("com.cloudinary:cloudinary-android:3.0.2")
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+
+    implementation("io.getstream:stream-video-android-core:1.9.2")
+    implementation("io.getstream:stream-video-android-ui-compose:1.9.2")
+
+
 }
